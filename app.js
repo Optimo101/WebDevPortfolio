1743a0ddd45f21167c5850d8358d655e3958c9b8
Justin <schiller.justin@gmail.com>
2019-02-25 12:11:32 -0600

    Add basic layout/function of Contact section. Begin Footer section.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require("express"),
 
 //============================================================
 app.use(express.static(__dirname + "/public"));
+app.use('/scripts', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/portfoliodb", {useNewUrlParser: true});
 
