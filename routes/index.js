7390dccdb9fd1df8b68b4788ddf640255afc4449
Justin <schiller.justin@gmail.com>
2019-02-27 20:59:19 -0600

    First attempt at adding server-side validation for contact form.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var express = require("express");
 var router = express.Router();
 var request = require("../models/request");
 
+var {body, validationResult} = require("express-validator/check");
 
 //                      ROUTES
 //==============================================================
@@ -13,8 +14,20 @@ router.get("/", function(req, res){
 });
 
 // CREATE CONTACT REQUEST
-router.post("/", function(req, res){
-    var contactRequest = {name: req.body.name, email: req.body.email, message: req.body.message};
+router.post("/", [
+    body('name', 'Empty name').isLength({ min: 1 }),
+    body('email', 'Empty email').isLength({min: 1}),
+    body('message', 'Empty message').isLength({min: 1})
+], function(req, res){
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+    var contactRequest = {
+        name: req.body.name, 
+        email: req.body.email, 
+        message: req.body.message
+    };
     request.create(contactRequest, function(err, dbmessage){
         if(err){
             console.log(err);
@@ -23,5 +36,7 @@ router.post("/", function(req, res){
         }
     });
 });
+
+
 
 module.exports = router;
