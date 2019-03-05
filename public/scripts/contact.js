d4bd5db08c0cf64545e55f321ca01817902320b5
Justin <schiller.justin@gmail.com>
2019-03-05 14:07:46 -0600

    Completing server-side validation with form error/confirm messages

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,7 +1,25 @@
-var contactSendButton = document.querySelector("#contactSendButton");
 
-contactSendButton.addEventListener("click", function(){
-    alert("Your message has been sent. Thank you.");
-});
+
+// window.onload = function() {
+//     document.getElementById('nameId').value = null;
+//     document.getElementById('nameId').placeholder = "Name";
+
+//     document.getElementById('emailId').value = null;
+//     document.getElementById('emailId').placeholder = "Email";
+
+//     document.getElementById('messageId').value = null;
+//     document.getElementById('messageId').placeholder = "Your message...";
+// }
+
+
+// function submitForm() {
+//     var f = document.getElementsById("contactForm")[0];
+//     if(f.checkValidity()) {
+//       f.submit();
+//       alert("Your message was sent.");
+//     } else {
+//       alert(document.getElementById('name').validationMessage);
+//     }
+//   }
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,40 +3,74 @@
 var express = require("express");
 var router = express.Router();
 var request = require("../models/request");
-
-var {body, validationResult} = require("express-validator/check");
+var validator = require("validatorjs");
 
 //                      ROUTES
 //==============================================================
 // INDEX 
 router.get("/", function(req, res){
-    res.render("index.ejs");
+    res.render("index.ejs",
+    {  
+        success: req.session.success,
+        posted: req.session.posted,
+        userName: req.session.name, 
+        userEmail: req.session.email, 
+        userMessage: req.session.message,
+        hasErrorName: req.session.hasErrorName,
+        hasErrorEmail: req.session.hasErrorEmail,
+        hasErrorMessage: req.session.hasErrorMessage,
+        errorName: req.session.errorName,
+        errorEmail: req.session.errorEmail,
+        errorMessage: req.session.errorMessage
+    });
+    req.session.success = false;
+    req.session.posted = false;
 });
 
 // CREATE CONTACT REQUEST
-router.post("/", [
-    body('name', 'Empty name').isLength({ min: 1 }),
-    body('email', 'Empty email').isLength({min: 1}),
-    body('message', 'Empty message').isLength({min: 1})
-], function(req, res){
-    var errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()});
-    }
-    var contactRequest = {
+router.post("/post", function(req, res){
+    req.session.posted = true;
+
+    let formData = {
         name: req.body.name, 
         email: req.body.email, 
         message: req.body.message
     };
-    request.create(contactRequest, function(err, dbmessage){
-        if(err){
-            console.log(err);
-        } else {
-            res.redirect("/");
-        }
-    });
+    let rules = {
+        name: 'required|string',
+        email: 'required|string|email',
+        message: 'required|string'
+    };
+
+    req.session.name = formData.name;
+    req.session.email = formData.email;
+    req.session.message = formData.message;
+
+    const validation = new validator(formData, rules);
+
+    if (validation.fails()) {
+        req.session.success = false;
+
+        req.session.hasErrorName = validation.errors.has("name");
+        req.session.hasErrorEmail = validation.errors.has("email");
+        req.session.hasErrorMessage = validation.errors.has("message");
+
+        req.session.errorName = validation.errors.first("name");
+        req.session.errorEmail = validation.errors.first("email");
+        req.session.errorMessage = validation.errors.first("message");
+
+        res.redirect("/");
+
+    } else {
+        req.session.success = true;
+        request.create(formData, function(err, dbmessage){
+            if(err){
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        });
+    }
 });
-
-
 
 module.exports = router;
