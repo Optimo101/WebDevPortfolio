769a68c90ba906c4fd039867dd4f09df0b05e60e
Jared Schiller <jaredpschiller@gmail.com>
2019-04-30 22:03:13 -0500

    Adding grid element and calculating grid columns/rows

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,11 +1,22 @@
-var landingButton = document.getElementsByClassName('landing-button')[0];
-var landingButtonIcon = document.getElementsByClassName('fa-chevron-circle-right')[0];
+//Get the grid container
+var landingGridElement = document.getElementById('landing-grid');
 
-function toggle90(){
-    landingButtonIcon.classList.toggle('turn-90-deg');
-    console.log('hi');
+
+
+function recalcLandingGrid() {
+   var Grid = {};
+   Grid.width = Number(window.getComputedStyle(landingGridElement).width.match(/\d+/));
+   Grid.height = Number(window.getComputedStyle(landingGridElement).height.match(/\d+/));
+   Grid.numOfGridColumns = 12; //hard-coded in landing.css
+   Grid.gridGap = 5 //px //hard-coded in landing.css
+   Grid.gridCellWidth = (Grid.width - (Grid.gridGap * (Grid.numOfGridColumns - 1))) / Grid.numOfGridColumns; 
+   Grid.numOfGridRows = Math.floor((Grid.height + Grid.gridGap) / (Grid.gridCellWidth + Grid.gridGap));
+   
+   
+   landingGridElement.style.gridTemplateRows = `repeat(${Grid.numOfGridRows}, ${Grid.gridCellWidth}px)`;
 }
 
-landingButton.addEventListener('mouseenter', toggle90);
-landingButton.addEventListener('mouseleave', toggle90);
+
+window.onload = recalcLandingGrid;
+window.onresize = recalcLandingGrid;
 
